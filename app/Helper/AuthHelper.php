2750bd23885302b9db5d3e4a40439541cc1c1db6
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-09-11 18:40:17 +0530

    Add login, register and logout func in AuthHelper

--- app/Helper/AuthHelper.php
+++ app/Helper/AuthHelper.php
@@ -1,11 +1,77 @@
 <?php
 
 namespace App\Helper;
+use DB;
+use Hash;
+use App\Models\User;
+use Illuminate\Contracts\Session\Session;
 
 class AuthHelper
 {
-    public function test()
+    protected $session;
+
+    // construct function
+    public function __construct(Session $session)
     {
-        echo "Testing is doing here";
+        $this->session = $session;
+    }
+
+    // login function
+    public function login(array $data)
+    {
+        $checkUserExist = User::where('email', $data['email'])->first();
+        if (!$checkUserExist) {
+            return [
+                'success' => false,
+                'message' => 'Email or Password not valid'
+            ];
+        }
+        if (!Hash::check($data['password'], $checkUserExist->password)) {
+            return [
+                'success' => false,
+                'message' => 'Incorrect Password'
+            ];
+        }
+        $this->session->put("user", $checkUserExist);
+        $this->session->migrate(true);
+        $this->user = $checkUserExist;
+        return [
+            'success' => true,
+            'message' => $checkUserExist->password
+
+        ];
+    }
+
+    // register function
+    public function register(array $data): array
+    {
+        DB::beginTransaction();
+        try {
+            $formData = [
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'username' => $data['username'],
+                'password' => Hash::make($data['password']),
+                'phone_number' => $data['phone_number']
+            ];
+
+            User::create($formData);
+            DB::commit();
+            return [
+                'success' => true,
+                'message' => 'User created successfully'
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return [
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+
+    public function logout(): void
+    {
+        $this->session->remove("user");
     }
 }
